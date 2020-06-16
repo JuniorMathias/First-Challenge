d984593cfbb15d334b9f91be5a991a7e4bb0b1c0
Mathias <juniormathiias@gmail.com>
2020-06-16 19:50:36 +0100

    first commit

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1` 
+    color: #FF0000; 
+
+    span {
+        color: blue;
+    }
+    `;
+
+    export const Titulo = styled.h2 `
+        color: #c1c1c1 ;
+    `;
+
+
